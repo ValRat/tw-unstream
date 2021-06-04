45448518176aee644c22402c9b0d4824df60be40
Valerian Ratu <valerian.ratu@orbis.com>
2021-06-04 16:16:20 -0700

    feat: better handling of indented points

--- tw-unstream.js
+++ tw-unstream.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const tiddlersFileLocation = '/home/valerian/Programming/tw-unstream/tiddlers.json'
-const flattenedFileLocation = '/home/valerian/Programming/tw-unstream/tiddlers_flattened.json'
+const tiddlersFileLocation = './tiddlers.json'
+const flattenedFileLocation = './tiddlers_flattened.json'
 
 const rawData = fs.readFileSync(tiddlersFileLocation, 'utf8');
 const entries = JSON.parse(rawData);
@@ -19,26 +19,52 @@ console.log('Stitching tiddlers...');
 // We want to only have parents, i.e. entry.parent == null
 const flattenedOutput = []
 for (const entry of entries) {
-  const isParent = entry.parent == null;
-  if (isParent) {
+  const isRoot = entry.parent == null;
+  if (isRoot) {
     const newEntry = {};
     newEntry.title = entry.title;
+    newEntry.type = entry.type;
     newEntry.tags = entry.tags;
     newEntry.modified = entry.modified;
     newEntry.created = entry.created;
-    newEntry.text = '';
+    newEntry.text = getChildText(entry, 1)
 
-    const children = entry['stream-list'];
-    if (children) {
-      for (const child of children.split(' ')) {
-        const childEntry = resultDictionary[child.trim()];
-        if (childEntry) {
-          newEntry.text += childEntry.text + '\n\n'; // needs two apparently
-        }
-      }
-      flattenedOutput.push(newEntry);
+    flattenedOutput.push(newEntry);
+  }
+}
+
+function getChildText(node, level) {
+
+  let output = '';
+
+  if (node == null) {
+    return output;
+  }
+
+  // PNGs and such, just delete them
+  if (node.type != null) {
+    return output
+  }
+
+  if (node.text && node.text.trim()) {
+    let depth = level;
+    while (depth) {
+      output += '*';
+      depth--;
+    }
+    output += ` ${node.text}\n`;
+    level += 1;
+  }
+
+  const children = node['stream-list'];
+  if (children) {
+    for (const child of children.split(' ')) {
+      const childEntry = resultDictionary[child.trim()];
+      output += getChildText(childEntry, level);
     }
   }
+
+  return output;
 }
 
 console.log('Writing file output...');
